43bc21738efbfbade201b8f377d2a83e474c1b69
PhucGG <phucggreal1@gmail.com>
2025-06-15 01:59:45 +0700

    Delete src directory

--- src/main/java/com/example/SWPP/repository/SurveyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SWPP.repository;
-
-import com.example.SWPP.entity.Survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyRepository extends JpaRepository<Survey, Integer> {
-}
--- src/main/java/com/example/SWPP/repository/SurveyTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SWPP.repository;
-
-import com.example.SWPP.entity.SurveyType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyTypeRepository extends JpaRepository<SurveyType, Integer> {
-}
